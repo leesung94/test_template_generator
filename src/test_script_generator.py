b3eb933bc680a673540b8509e38fc5ea2dd0ef98
Lee <leesung1994@hotmail.co.uk>
2019-11-06 18:20:54 +0000

    Refactored test script

--- src/test_script_generator.py
+++ src/test_script_generator.py
@@ -3,6 +3,13 @@ import unittest
 import script_generator as sg
 
 class ContentExtractor(unittest.TestCase):
+
+    def create_basic_inst(self, filepath):
+        inst = sg.Contents_Extractor(filepath)
+        inst.open_file()
+        inst.read_contents()
+        inst.close_file()
+        return inst
     
     def test_create_object(self):
         inst = sg.Contents_Extractor("../test/class.m")
@@ -22,44 +29,34 @@ class ContentExtractor(unittest.TestCase):
         self.assertEquals(result, 1)
 
     def test_exist_substring(self):
-        inst = sg.Contents_Extractor("../test/class.m")
-        inst.open_file()
-        inst.read_contents()
-        inst.close_file()
+        inst = self.create_basic_inst("../test/class.m")
         result = inst.match_first_item_in_contents("classdef")
         self.assertEquals(result, 0)
 
     def test_nonexist_substring(self):
-        inst = sg.Contents_Extractor("../test/class.m")
-        inst.open_file()
-        inst.read_contents()
-        inst.close_file()
+        inst = self.create_basic_inst("../test/class.m")
         result = inst.match_first_item_in_contents("nonexistence")
         self.assertEquals(result, None)
 
     def test_no_start_exist_substring(self):
-        inst = sg.Contents_Extractor("../test/func2.m")
-        inst.open_file()
-        inst.read_contents()
-        inst.close_file()
+        inst = self.create_basic_inst("../test/func2.m")
         result = inst.match_first_item_in_contents("function")
         self.assertEquals(result, 1)
 
     def test_class_get_script_type(self):
-        inst = sg.Contents_Extractor("../test/class.m")
-        inst.open_file()
-        inst.read_contents()
-        inst.close_file()
+        inst = self.create_basic_inst("../test/class.m")
         result = inst.get_script_type()
         self.assertTrue(result)
 
     def test_class_get_script_type(self):
-        inst = sg.Contents_Extractor("../test/func1.m")
-        inst.open_file()
-        inst.read_contents()
-        inst.close_file()
+        inst = self.create_basic_inst("../test/func1.m")
         result = inst.get_script_type()
         self.assertFalse(result)
+
+    def test_class_get_script_type(self):
+        inst = self.create_basic_inst("../test/notscript.m")
+        result = inst.get_script_type()
+        self.assertEquals(result, None)
 
 class ScriptGenerator(unittest.TestCase):
 
