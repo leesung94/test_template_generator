2be59538e66798d25cb97840d7f8d9afd120f535
Lee <leesung1994@hotmail.co.uk>
2019-11-06 20:12:41 +0000

    Implemented template generation. Function names are now returned instead of full lines

--- src/script_generator.py
+++ src/script_generator.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*- 
 
 # Author:
 #   Lee Sung
@@ -13,7 +14,7 @@ class Contents_Extractor:
         self.filehandle = None
         self.directory = None
         self.is_class = None
-        self.function_lines = []
+        self.function_names = []
         self.first_line = None
 
     def open_file(self):
@@ -30,13 +31,24 @@ class Contents_Extractor:
     def read_contents(self):
         self.contents = self.filehandle.readlines()
 
-    def get_function_lines(self):
+    def get_function_names(self):
         # Exclude first line incase the script is a function script
         modified_list = self.contents[self.first_line:]
         # Witchcraft TM
         # Gets all lines in the string array which contain the word "function"
-        self.function_lines = [line for line in modified_list if "function" in line]
-        return self.function_lines
+        function_lines = [line for line in modified_list if "function" in line]
+        for func in function_lines:
+            # Want everything left of the bracket
+            firstpass = func.split("(")[0]
+            secondpass = ""
+            # Want what is left of the equals sign and space
+            if ("= " in firstpass):
+                secondpass = firstpass.split("= ")[1]
+            elif ("=" in firstpass):
+                secondpass = firstpass.split("=")[1]
+            else:
+                secondpass = firstpass.split("function ")[1]
+            self.function_names.append(secondpass)
 
     def match_first_item_in_contents(self, substring):
         """
@@ -70,13 +82,55 @@ class Script_Generator:
 
     def __init__(self, filename, parent_dir):
         self.filename = "TEST_" + filename
-        self.file_contents = []
-        self.parent_dir = parent_dir
+        self.classname = os.path.splitext(filename)[0]
+        self.file_contents = None
+        self.parent_dir = os.path.join(parent_dir, '')
+        self.fileloc = self.parent_dir + self.filename
 
     def generate_file_head(self):
-        self.file_contents.append("classdef (TestTags = {''}) " + self.filename + " < matlab.unittest.TestCase")
-        self.file_contents.append("\n\n\n")
-        print self.file_contents
+        self.file_contents = ("classdef (TestTags = {''}) " + self.classname + " < matlab.unittest.TestCase\n\n")
+        self.file_contents += ("    properties\n\n")
+        self.file_contents += ("    end\n\n")
+        self.file_contents += ("    methods (TestClassSetup)\n\n")
+        self.file_contents += ("        function setup(testCase)\n\n")
+        self.file_contents += ("        end\n\n")
+        self.file_contents += ("    end\n\n")
+        self.file_contents += ("    methods (TestClassTeardown)\n\n")
+        self.file_contents += ("        function teardown(testCase)\n\n")
+        self.file_contents += ("        end\n\n")
+        self.file_contents += ("    end\n\n")
+        self.file_contents += ("    methods (TestMethodTeardown)\n\n")
+        self.file_contents += ("    end\n\n")
+        self.file_contents += ("    methods (Test)\n\n")
+
+    def generate_function_block(self, function_name):
+        self.file_contents += ("        function test_" + function_name + "(testCase)\n\n")
+        self.file_contents += ("        % Test Objective:\n")
+        self.file_contents += ("        % -------------------------------------------------------------\n\n")
+        self.file_contents += ("        % Pass Criteria:\n")
+        self.file_contents += ("        % -------------------------------------------------------------\n\n")
+        self.file_contents += ("        % Test Input Data:\n")
+        self.file_contents += ("        % -------------------------------------------------------------\n\n")
+        self.file_contents += ("        % Execute Unit Under Test:\n")
+        self.file_contents += ("        % -------------------------------------------------------------\n\n")
+        self.file_contents += ("        % Verification:\n")
+        self.file_contents += ("        % -------------------------------------------------------------\n\n")
+        self.file_contents += ("        end\n\n")
+
+    def generate_file_tail(self):
+        self.file_contents += ("    end\n\n")
+        self.file_contents += ("    methods\n\n")
+        self.file_contents += ("    end\n\n")
+        self.file_contents += ("end\n\n")
+
+    def write_file(self):
+        if os.path.isfile(self.fileloc):
+            print "Test Template already exists in: " + self.fileloc
+        else:
+            file_handle = open(self.fileloc, "w+") 
+            file_handle.write(self.file_contents)
+            file_handle.close()
+
 
     
 
